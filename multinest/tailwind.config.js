132e701b2646aa5b99fdbabe0ed10a6f0eb88f5f
Ehsan <ehsan.fathi2195@gmail.com>
2025-10-20 23:15:40 +0330

    starting the website

--- multinest/tailwind.config.js
+++ multinest/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bricolage: ['"Bricolage Grotesque"', 'sans-serif'],
+        Jakarta: ['"Plus Jakarta sans", sans-serif']
+      }
+    },
   },
   plugins: [],
 }
